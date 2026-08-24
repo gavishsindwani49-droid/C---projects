bd86fb823735b44c25dff63fef26c4fc25eca629
Gavish <gavishsindwani49@gmail.com>
2026-08-24 19:05:31 +0530

    git commit

---
+++ foreachloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    std::string students[] = {"Spongebob", "Patrick", "Squidward", "lol"};
+
+    for(std::string student : students){
+        std::cout << student << '\n';
+    }
+
+
+    return 0;
+}  
